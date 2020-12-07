14715a7a69c43e3df6182dd59d92c1f3ed506cb5
Haruaki Kobayashi <rigelpain@gmail.com>
2020-12-07 19:30:36 +0900

    [update] wordpress 引っ越し中

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+  <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/slick/slick.min.js"></script>
+  <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
+  <?php wp_footer(); ?>
+</footer>
---
+++ header.php
@@ -0,0 +1,17 @@
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/slick/slick.css" />
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/slick/slick-theme.css" />
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/common.css" />
+    <link
+      href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c|Varela+Round&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      href="https://use.fontawesome.com/releases/v5.6.1/css/all.css"
+      rel="stylesheet"
+    />
+    <title>Rigel paiN</title>
+    <?php wp_head(); ?>
+  </head>
